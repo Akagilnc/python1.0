e4cb97ed183233ec8a2f7a886d2476f9e5990a38
DeronLee <akagilnc@gmail.com>
2020-04-04 23:43:32 +0900

    l5 finished

--- lesson5.py
+++ lesson5.py
@@ -3,47 +3,44 @@ import pandas as pd
 import os
 
 
-def get_data_from_json(file_name):
+# 读取json文件的数据
+def read_json_file(file_name):
     with open(file_name, encoding='utf_8') as file:
         return json.load(file)
 
 
-def save_data_to_excel():
-    filenames = os.listdir('./json_files')
-    writer = pd.ExcelWriter('./excel_files/report.xlsx', engine='openpyxl')
+# 把数据依次的存储到excel里
+def save_to_excel():
+    # 获取文件名
+    file_names = os.listdir('./json_files')
+    # 初始化汇总的信息
     sum_df = pd.DataFrame()
-    for name in filenames:
-        # 从 文件名中截取洲名
-        tag_name = name[:name.index('.')]
-        # 将文件名添加上路径，用于读取json文件
-        file_name = './json_files/{}'.format(name)
-        # 从json文件中获取数据
-        data_json = get_data_from_json(file_name)
+    # 生成我们最终要保存的excel
+    with pd.ExcelWriter('./excel_files/report.xlsx', engine='openpyxl') as excel_file:
+        for file_name in file_names:
+            # 抓取大洲的名字
+            sheet_name = file_name[:-5]
+            # 获取json的信息
+            file_name = './json_files/{}'.format(file_name)
+            data = read_json_file(file_name)
 
-        # 将数据转换为DataFrame，并指定需要的columns
-        df = pd.DataFrame(data_json, columns=['locationId', 'continentName', 'continentEnglishName',
-                                              'countryName', 'countryEnglishName', 'provinceName',
-                                              'provinceEnglishName', 'provinceShortName', 'currentConfirmedCount',
-                                              'confirmedCount', 'suspectedCount', 'curedCount', 'deadCount', 'comment'])
+            # 转换json到pandas.DataFrame
+            columns_names = ['continentName', 'countryName', 'provinceName', 'currentConfirmedCount',
+                             'confirmedCount', 'curedCount', 'deadCount']
+            df = pd.DataFrame(data, columns=columns_names, index=range(1, len(data)+1))
+            sum_columns_list = ['currentConfirmedCount', 'confirmedCount', 'curedCount', 'deadCount']
+            # 从原始的df选出四列，求和
+            sum_info = df[sum_columns_list].sum()
+            # 给sum_info 取了个名字，大洲名字+汇总
+            sum_info = sum_info.rename('{}汇总'.format(sheet_name))
+            sum_df = sum_df.append(sum_info)
+            # 保存到excel文件
+            df.to_excel(excel_file, index=True, sheet_name=sheet_name)
 
-        # 生成汇总信息，rename为洲名-汇总并存储到DateFrame里面
-        sum_info = df[['currentConfirmedCount', 'confirmedCount',
-                       'suspectedCount', 'curedCount', 'deadCount']].sum()
-        sum_info = sum_info.rename('{}-汇总'.format(tag_name))
-        sum_df = sum_df.append(sum_info)
-
-        # 保存到独立的文件
-        df.to_excel('./excel_files/{}.xlsx'.format(tag_name), index=False)
-        df.to_csv('./csv_files/{}.csv'.format(tag_name), index=False)
-
-        # 保存到report文件的独立sheet
-        df.to_excel(writer, sheet_name=tag_name, index=False)
-
-    # 保存汇总信息到汇总sheet
-    sum_df.to_excel(writer, sheet_name='汇总', index=False)
-    sum_df.to_csv('./csv_files/report.csv', index=False)
-
-    writer.save()
+        sum_df.to_excel(excel_file, sheet_name='汇总')
+        string_sum = sum_df.to_string()
+        with open('report.txt', 'w', encoding='utf-8') as file:
+            file.write(string_sum)
 
 
-save_data_to_excel()
+save_to_excel()
---
+++ lesson5_quiz.md
@@ -0,0 +1,89 @@
+# Lesson5 Quiz
+
+1. 如何初始化一个DataFrame, 
+    ```python
+        import pandas as pd
+    
+        df = DataFrame()
+    
+        df = pd.dataFrame()
+    
+        df = pd.DataFrame()
+    
+        df = pd.Dataframe()
+    ```
+    
+1. 对于 `import pandas as pd` 以下说法正确的是？
+    
+    引入了pandas这个外部包
+    
+    引入了pd这个外部包
+    
+    调用需要写pandas.xxx()
+    
+    调用需要写pd.xxx()   
+    
+1. 对于一个 DataFrame df , df['x']是在做什么？
+    
+    取得key为'x'的value
+    
+    取第x个元素
+    
+    取包含'x'的列
+    
+    取列名为'x'的列
+
+1. 对于DataFrame.append()的描述正确的为：
+    
+    df.append(x)即可把x添加到df里面去
+    
+    df.append()可以把一个df添加进另外一个df
+    
+    df.append()没有返回值
+    
+    df.append()和list.append()的工作方式不同
+
+1. 对于初始化 DataFrame df 时， `df = pd.DataFrame(data, columns=['1', '2', '3'])` 是在做什么
+
+    新建3列，列名分别为1, 2, 3
+    
+    从原始数据中，只拿列名为1, 2, 3的
+    
+    从原始数据中，只拿行名为'1', '2', '3'的
+    
+    从原始数据中，找到列名为'1', '2', '3'的
+    
+1. 对于已知的`DataFrame df，df.sum()` 的描述正确的是？
+    
+    只能对一列数字(整数或浮点数或其它数据类型)求和
+    
+    只能对所有数字(整数或浮点数或其它数据类型)的列求和
+    
+    对所有的行求和,但是忽略包含NaN的列
+    
+    对所有的列尝试求和，包括NaN的列
+    
+    最终返回的是一个Series
+
+1. `df.to_excel()` 的描述正确的是？
+
+    第一个参数可以传文件名，但是必须要是'xlsx'结尾的
+    
+    `index=True` 参数可以去掉自动生成的数字参数
+    
+    第一个参数可以传入一个pd.ExcelWriter
+    
+    第一个参数可以用`'{}.json'.format(x)`的方式传入
+
+1. 对于`writer = pd.ExcelWriter('x.xlsx', engine='openpyxl')`，以下描述正确的是：
+
+    之后我可能会把数据存入到x.xlsx的默认sheet里面
+    
+    后续我可以在x.xlsx文件中多次写入数据
+    
+    writer有可能被我用来作为`df.to_csv()`的参数
+
+    当我调用`df.to_excel(writer)`时，我必须还需要加上`sheet_name='xxx'`这个参数才可以成功
+
+
+
