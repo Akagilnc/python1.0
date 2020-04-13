c74762c55d065bbcff85e84d08f07989e8b6081a
DeronLee <akagilnc@gmail.com>
2020-04-13 22:41:30 +0900

    nothing

--- lesson5_hw.py
+++ lesson5_hw.py
@@ -2,8 +2,8 @@ import json
 import pandas as pd
 
 
-def read_china_area_data_from_json(file_name_with_path):
-    with open(file_name_with_path, encoding='utf_8') as file:
+def read_china_area_data_from_json(file_name):
+    with open(file_name, encoding='utf_8') as file:
         return json.load(file).get('data').get('list')
 
 
@@ -12,34 +12,34 @@ def save_to_excel(input_df, writer, sheet_name, index=False):
     writer.save()
 
 
+def rename_and_append(df, area_name, df_list):
+    series_list = [df.sum(), df.mean(), df.median()]
+    for index, series in enumerate(series_list):
+        series = series.rename(area_name)
+        df_list[index] = df_list[index].append(series)
+    return df_list
+
+
 def get_area_info(input_data):
     report = pd.ExcelWriter('lesson5_hw_report.xlsx', engine='openpyxl')
     sum_df = mean_df = mid_df = pd.DataFrame()
     sheet_names_stat = ['汇总', '均值', '中位数']
-    df_list = [sum_df, mean_df, mid_df]
+    stat_df_list = [sum_df, mean_df, mid_df]
+
     for area_info in input_data:
         area_name = area_info.get('name')
         area_cities = area_info.get('city')
         df = pd.DataFrame(area_cities, columns=['name', 'conNum', 'cureNum',
-                                                'deathNum', 'econNum', 'zerodays'])
+                                                'deathNum', 'econNum'])
+        columns_need_to_be_int = ['conNum', 'cureNum', 'deathNum', 'econNum']
+        df[columns_need_to_be_int] = df[columns_need_to_be_int].astype('int')
 
-        columns_need_to_be_int_names = ['conNum', 'cureNum', 'deathNum', 'econNum', 'zerodays']
-        df = df[columns_need_to_be_int_names].astype('int')
-
-        df_list = rename_and_append(df, area_name, df_list)
+        stat_df_list = rename_and_append(df[columns_need_to_be_int], area_name, stat_df_list)
 
         save_to_excel(df, report, area_name)
 
-    for index, df in enumerate(df_list):
-        save_to_excel(df, report, sheet_names_stat[index], True)
-
-
-def rename_and_append(df, area_name, df_list):
-    series_list = [df.sum(), df.mean(), df.median()]
-    for index, info in enumerate(series_list):
-        info = info.rename(area_name)
-        df_list[index] = df_list[index].append(info)
-    return df_list
+    for index, stat_df in enumerate(stat_df_list):
+        save_to_excel(stat_df, report, sheet_names_stat[index], True)
 
 
 data = read_china_area_data_from_json('lesson4.json')
--- lesson6.py
+++ lesson6.py
@@ -1,69 +1,57 @@
 # 数据bug
 # 中国疫情地区详情
 # 确诊排序
-# top10国家排名
 # 列名
+# top10国家排名
 import pandas as pd
 
 
-def sort_by_column(data, column_name, ascending=False):
-    return data.sort_values(by=column_name, ascending=ascending)
+# 数据bug修复
+def fix_issues_from_boss():
+    # 读取excel文件的亚洲sheet
+    data = pd.read_excel('./excel_files/report.xlsx', sheet_name=None)
+    # 初始化新的报告文件
+    writer = pd.ExcelWriter('./excel_files/report_fixed.xlsx', engine='openpyxl')
 
-
-def save_to_excel(data, sheet_name, writer, index_flag=False):
-    data.to_excel(writer, sheet_name=sheet_name, index=index_flag)
+    # 依次拿出所有的数据
+    for key, df in data.items():
+        if key == '亚洲':
+            # 找到中国的地区信息
+            df_china_details = df[(df['countryName'] == '中国') & (df['provinceName'] != '中国')]
+            # 把它从原始数据中删除
+            df = df.drop(df_china_details.index)
+            df_china_details = df_china_details.sort_values(by='confirmedCount', ascending=False)
+            df_china_details.to_excel(writer, sheet_name='中国', index=False)
+        # 新的亚洲数据，保存到新的report
+        df = df.sort_values(by='confirmedCount', ascending=False)
+        df = df.rename(columns={'confirmedCount': '累计确诊', 'currentConfirmedCount': '现存确诊'})
+        df.to_excel(writer, sheet_name=key, index=False)
     writer.save()
 
 
-# BUG 修复
-def fix_issue_from_boss():
-    # 读取excel文件，并且读取亚洲sheet
-    data = pd.read_excel('./excel_files/report.xlsx', sheet_name=None)
-    # 初始化报告文件
-    writer = pd.ExcelWriter('./excel_files/report_fixed.xlsx', engine='openpyxl')
-
-    # 依次读取每一个sheet的信息
-    for name, df in data.items():
-        sort_by_column_name = 'confirmedCount'
-        if name == '亚洲':
-            # 找到中国的地区信息，并且从原始数据中删除
-            data_china_details = df[(df['countryName'] == '中国') & (df['provinceName'] != '中国')]
-            # 生成新的亚洲数据
-            df = df.drop(data_china_details.index)  # 常用的，删除行和列的用法
-            # 排序并保存中国数据到excel
-            data_china_details = sort_by_column(data_china_details, sort_by_column_name)
-            save_to_excel(data_china_details, '中国', writer)
-
-        # 对已经生成好的df按照confirmedCount排序
-        df = sort_by_column(df, sort_by_column_name)
-        save_to_excel(df, name, writer)
-    writer.close()
-
-
 def make_top10_country():
-    # 读取excel文件，读取多个sheet。
-    all_df = pd.read_excel('./excel_files/report_fixed.xlsx', sheet_name=None)
-    # 删除国家信息之外的sheet信息
-    del all_df['汇总']
-    del all_df['中国']
-    # 确保每次生成的‘top10'为唯一
-    if 'top10' in all_df.keys():
-        del all_df['top10']
-    # 整和数据
-    all_df = pd.concat(all_df, ignore_index=True)
-    # 排序，并且找到top10的国家
-    all_df = sort_by_column(all_df, 'confirmedCount')
+    # 读取excel，读取多个sheet
+    all_df_dict = pd.read_excel('./excel_files/report_fixed.xlsx', sheet_name=None)
+
+    # 删除国家信息之外的数据，汇总和中国
+    del all_df_dict['汇总']
+    del all_df_dict['中国']
+    # 确保top10这个sheet是唯一的
+    if 'top10' in all_df_dict.keys():
+        del all_df_dict['top10']
+    # 整合数据
+    all_df = pd.concat(all_df_dict, ignore_index=True)
+    # 排序，找到top10的国家
+    all_df = all_df.sort_values(by='累计确诊', ascending=False)
     top10_df = all_df.head(10)
-    top10_df = top10_df.rename(columns={'confirmedCount': '累计确诊', 'currentConfirmedCount': '现存确诊',
-                                        'curedCount': '治愈人数', 'deadCount': '死亡人数',
-                                        'countryName': '国家'})
+    top10_df = top10_df.rename(columns={'countryName': '国家',
+                                        'curedCount': '治愈',
+                                        'deadCount': '死亡'})
     top10_df = top10_df.drop(columns=['provinceName', 'continentName'])
-    top10_df = top10_df.set_index('国家')
-
-    # 保存到report里面去
+    # 保存到report_fixed里面去
     with pd.ExcelWriter('./excel_files/report_fixed.xlsx', engine='openpyxl', mode='a') as writer:
-        save_to_excel(top10_df.T, 'top10', writer, True)
+        top10_df.to_excel(writer, sheet_name='top10', index=False)
 
 
-fix_issue_from_boss()
+fix_issues_from_boss()
 make_top10_country()
