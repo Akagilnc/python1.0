a4956469c69b3bbaf87e62b4470ba9616b3947bf
DeronLee <akagilnc@gmail.com>
2020-05-17 00:10:34 +0900

    3rd reviews lesson1

--- lesson1.py
+++ lesson1.py
@@ -1,29 +1,31 @@
 # 求圆的面积
-def print_round_area():
-    # 定义pi = 3.14
+def get_round_area(input_r):
+    # 定义pi的值
     pi = 3.14
-    # 获取用户输入的r
-    r = input('please input r: ')
-    r = float(r)
-    # pi * r 的平方
-    area = pi * r ** 2
-    print(area)
+    # 半径转换为float
+    input_r = float(input_r)
+    # 计算面积
+    area = pi * input_r ** 2
+    return area
 
 
-# 判断奇偶数
-def odd_or_even():
-    # 获取用户输入的数字
-    number = int(input('please input a number: '))
+# 定义一个判断奇偶数
+def odd_or_even(input_number):
+    # 把输入转换为int
+    input_number = int(input_number)
     # 判断奇偶
     # 如果是偶数
-    if number % 2 == 0:
-        # 打印 even
-        print('even')
+    if input_number % 2 == 0:
+        return 'even'
     # 否则
     else:
-        # 打印 odd
-        print('odd')
+        return 'odd'
+# # 获取用户输入的半径
+# r = input('input r: ')
+# result = get_round_area(r)
+# # 打印结果
+# print(result)
 
-
-print_round_area()
-odd_or_even()
+number = input('input a number: ')
+result = odd_or_even(number)
+print(result)
