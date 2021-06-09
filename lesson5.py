7ef0bf8101dd56ccd707af8cdfefd87989ca75dd
DeronLee <akagilnc@gmail.com>
2021-06-09 16:01:37 +0900

    202106

--- lesson5.py
+++ lesson5.py
@@ -12,9 +12,12 @@ def process_and_save_to_excel(filename):
     sum_df = pd.DataFrame()
     with pd.ExcelWriter('l5_report.xlsx') as file:
         result = read_json_file('{}.json'.format(filename))
-        content_df = pd.DataFrame(result)
-        cols = ['conNum', 'cureNum', 'deathNum']
-        content_df = content_df.astype({'conNum': int, 'cureNum': int, 'deathNum': int})
+        content_df = pd.DataFrame(result, columns=['name', 'conNum', 'cureNum', 'deathNum'])
+        content_df = content_df.rename(columns={'name': '名称', 'conNum': '确诊人数', 'cureNum': '治愈人数',
+                                        'deathNum': '死亡人数'})
+        cols = ['确诊人数', '治愈人数', '死亡人数']
+        print(content_df)
+        content_df = content_df.astype({'确诊人数': int, '治愈人数': int, '死亡人数': int})
         sum_df = sum_df.append(content_df[cols].sum(), ignore_index=True)
         content_df.to_excel(file, index=False,
                             sheet_name=filename)
@@ -28,4 +31,4 @@ file_names = [
     'l5_china_history',
     'l5_world_area_latest'
 ]
-process_and_save_to_excel('l5_oversea_area_latest')
+process_and_save_to_excel(file_names[2])
